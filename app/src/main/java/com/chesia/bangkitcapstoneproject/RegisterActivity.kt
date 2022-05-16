caa979230cafcb58183462651d8d8b8afcfee4c8
JSaii <38206432+JSaii@users.noreply.github.com>
2022-05-16 14:41:05 +0700

    Menambah Login page, Register Page, dan beberapa Resource
    Membuat Login Page,
    Membuat Register Page,
    Menambahkan beberapa drawables, colors, dan strings pada resource.

--- app/src/main/java/com/chesia/bangkitcapstoneproject/RegisterActivity.kt
+++ app/src/main/java/com/chesia/bangkitcapstoneproject/RegisterActivity.kt
@@ -5,16 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_register)
     }
 
-    fun intentRegister(view: View) {
-        val intent = Intent(this@MainActivity, RegisterActivity::class.java)
-        startActivity(intent);
+    fun intentMenu(view: View) {
+        val intent = Intent(this@RegisterActivity, MainActivity::class.java)
+        startActivity(intent)
     }
 }
