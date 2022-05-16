6febb01b672038416d8f90315c47df1637c611ce
ailatan aisehc <89681954+chesianatalia@users.noreply.github.com>
2022-05-16 23:43:28 +0700

    adding onClickListener
    to see the flow

--- app/src/main/java/com/chesia/bangkitcapstoneproject/MainActivity.kt
+++ app/src/main/java/com/chesia/bangkitcapstoneproject/MainActivity.kt
@@ -2,10 +2,36 @@ package com.chesia.bangkitcapstoneproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+<<<<<<< Updated upstream
+=======
+import android.view.View
+import com.chesia.bangkitcapstoneproject.databinding.ActivityMainBinding
+>>>>>>> Stashed changes
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+<<<<<<< Updated upstream
         setContentView(R.layout.activity_main)
+=======
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btLogin.setOnClickListener{
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
+
+    fun intentRegister(view: View) {
+        val intent = Intent(this@MainActivity, RegisterActivity::class.java)
+        startActivity(intent);
+>>>>>>> Stashed changes
+    }
+
+
 }
