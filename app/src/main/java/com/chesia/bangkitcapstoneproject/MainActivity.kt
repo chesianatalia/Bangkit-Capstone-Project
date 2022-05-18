9ef499529512721e4de1a9d232c215783f7bbe47
ailatan aisehc <89681954+chesianatalia@users.noreply.github.com>
2022-05-18 18:57:13 +0700

    trying to styling cameraX

--- app/src/main/java/com/chesia/bangkitcapstoneproject/MainActivity.kt
+++ app/src/main/java/com/chesia/bangkitcapstoneproject/MainActivity.kt
@@ -8,22 +8,24 @@ import com.chesia.bangkitcapstoneproject.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btLogin.setOnClickListener{
+        binding.btLogin.setOnClickListener {
             val intent = Intent(this, DashboardActivity::class.java)
             startActivity(intent)
         }
-        
-    fun intentRegister(view: View) {
-        val intent = Intent(this@MainActivity, RegisterActivity::class.java)
-        startActivity(intent);
+
+        fun intentRegister(view: View) {
+            val intent = Intent(this@MainActivity, RegisterActivity::class.java)
+            startActivity(intent);
+
+        }
+
 
     }
-
-
 }
