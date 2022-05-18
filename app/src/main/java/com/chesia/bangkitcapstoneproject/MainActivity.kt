ad32e4dd58e4045739009ee0ab2adee356288182
hnsfhrzi <hans13fahrezi@gmail.com>
2022-05-18 18:36:06 +0700

    Adding Homepage
    Adding Homepage and some icon

--- app/src/main/java/com/chesia/bangkitcapstoneproject/MainActivity.kt
+++ app/src/main/java/com/chesia/bangkitcapstoneproject/MainActivity.kt
@@ -4,8 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+<<<<<<< Updated upstream
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+=======
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btLogin.setOnClickListener {
+            val intent = Intent(this, HomepageActivity::class.java)
+            startActivity(intent)
+        }
+
+        fun intentRegister(view: View) {
+            val intent = Intent(this@MainActivity, RegisterActivity::class.java)
+            startActivity(intent);
+
+        }
+
+>>>>>>> Stashed changes
     }
 }
