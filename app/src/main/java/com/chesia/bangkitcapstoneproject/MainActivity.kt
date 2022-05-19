d8df013c595b699799f1381aeb4dbd9550501f19
JSaii <38206432+JSaii@users.noreply.github.com>
2022-05-19 12:28:59 +0700

    Added edit profile page, and google sign in button

--- app/src/main/java/com/chesia/bangkitcapstoneproject/MainActivity.kt
+++ app/src/main/java/com/chesia/bangkitcapstoneproject/MainActivity.kt
@@ -8,10 +8,28 @@ import android.view.View
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+<<<<<<< Updated upstream
         setContentView(R.layout.activity_main)
 
 
     }
+=======
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btLogin.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+        }
+//             val intent = Intent(this, HomepageActivity::class.java)
+//             startActivity(intent)
+//         }
+    }
+
+    fun intentRegister(view: View) {
+        val intent = Intent(this@MainActivity, RegisterActivity::class.java)
+        startActivity(intent);
+>>>>>>> Stashed changes
 
     fun intentRegister(view: View) {
         val intent = Intent(this@MainActivity, RegisterActivity::class.java)
