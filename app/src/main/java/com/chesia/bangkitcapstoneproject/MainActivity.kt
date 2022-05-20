888fa98c0399bdb6dec2f6e44a278f4869020d34
JSaii <38206432+JSaii@users.noreply.github.com>
2022-05-20 21:41:01 +0700

    Implemented register and login

--- app/src/main/java/com/chesia/bangkitcapstoneproject/MainActivity.kt
+++ app/src/main/java/com/chesia/bangkitcapstoneproject/MainActivity.kt
@@ -1,10 +1,24 @@
 package com.chesia.bangkitcapstoneproject
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+<<<<<<< Updated upstream
+=======
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import com.chesia.bangkitcapstoneproject.Networking.ApiConfig
+import com.chesia.bangkitcapstoneproject.Networking.LoginResponse
+>>>>>>> Stashed changes
 import com.chesia.bangkitcapstoneproject.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,13 +26,45 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btLogin.setOnClickListener {
+<<<<<<< Updated upstream
             val intent = Intent(this, DashboardActivity::class.java)
             startActivity(intent)
+=======
+            hideSoftKeyboard(binding.root)
+            setProgressBar(true)
+            if(binding.edtEmail.text.isNotEmpty() && binding.edtPassword.text.isNotEmpty()){
+                val client = ApiConfig.getApiService().login(binding.edtEmail.text.toString(), binding.edtPassword.text.toString())
+                client.enqueue(object : Callback<LoginResponse>{
+                    override fun onResponse(
+                        call: Call<LoginResponse>,
+                        response: Response<LoginResponse>
+                    ) {
+                        setProgressBar(false)
+                        if(response.isSuccessful && response.body()!!.data != null){
+                            val intent = Intent(this@MainActivity, HomepageActivity::class.java);
+                            startActivity(intent)
+                            finish()
+                        }else{
+                            Toast.makeText(this@MainActivity, "An error occurred", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                        Toast.makeText(this@MainActivity, "An error occurred", Toast.LENGTH_SHORT).show()
+                        Log.d("NetworkError", "ERROR: ${t.message}")
+                    }
+
+                })
+            }else{
+                setProgressBar(false)
+                Toast.makeText(this, "Tolong isi semua form!", Toast.LENGTH_SHORT).show()
+            }
+
+>>>>>>> Stashed changes
         }
 //             val intent = Intent(this, HomepageActivity::class.java)
 //             startActivity(intent)
@@ -29,4 +75,36 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this@MainActivity, RegisterActivity::class.java)
         startActivity(intent);
     }
+<<<<<<< Updated upstream
+=======
+
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        supportActionBar?.hide()
+
+    }
+
+    private fun hideSoftKeyboard(view: View){
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun setProgressBar(loading: Boolean){
+        if(loading){
+            binding.pbLogin.visibility = View.VISIBLE
+        }else{
+            binding.pbLogin.visibility = View.GONE
+        }
+    }
+>>>>>>> Stashed changes
 }
--- app/src/main/java/com/chesia/bangkitcapstoneproject/RegisterActivity.kt
+++ app/src/main/java/com/chesia/bangkitcapstoneproject/RegisterActivity.kt
@@ -1,18 +1,103 @@
 package com.chesia.bangkitcapstoneproject
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import com.chesia.bangkitcapstoneproject.Networking.ApiConfig
+import com.chesia.bangkitcapstoneproject.Networking.RegisterResponse
+import com.chesia.bangkitcapstoneproject.databinding.ActivityMainBinding
+import com.chesia.bangkitcapstoneproject.databinding.ActivityRegisterBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btRegister.setOnClickListener{
+            hideSoftKeyboard(binding.root)
+            setProgressBar(true)
+            if(checkForm()){
+                val client = ApiConfig.getApiService().register(
+                    binding.edtRegisterNama.text.toString(),
+                    binding.edtRegisterEmail.text.toString(),
+                    binding.edtRegisterPassword.text.toString())
+
+                client.enqueue(object : Callback<RegisterResponse>{
+                    override fun onResponse(
+                        call: Call<RegisterResponse>,
+                        response: Response<RegisterResponse>
+                    ) {
+                        if(response.isSuccessful && response.body()!= null){
+                            setProgressBar(false)
+                            Toast.makeText(this@RegisterActivity, "Account successfully registered!", Toast.LENGTH_SHORT).show()
+                            intentMenu(binding.root)
+                        }else{
+                            Toast.makeText(this@RegisterActivity, "An error occurred", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onFailure(call: Call<RegisterResponse>, t: Throwable) {
+                        setProgressBar(false)
+                        Toast.makeText(this@RegisterActivity, "An error occurred", Toast.LENGTH_SHORT).show()
+                        Log.d("NETWORK", "ERROR: ${t.message}")
+                    }
+
+                })
+            }
+        }
     }
 
     fun intentMenu(view: View) {
         val intent = Intent(this@RegisterActivity, MainActivity::class.java)
         startActivity(intent)
+        finish()
+    }
+
+    private fun checkForm(): Boolean{
+        if(binding.edtRegisterNama.text.isNotEmpty()
+            && binding.edtRegisterEmail.text.isNotEmpty()
+            && binding.edtRegisterPassword.text.isNotEmpty()
+            && binding.edtRegisterPassword2.text.isNotEmpty()
+            && binding.edtRegisterTelp.text.isNotEmpty())
+            {
+                if(binding.edtRegisterPassword.text.toString() == binding.edtRegisterPassword2.text.toString()){
+                    return true
+                }else{
+                    setProgressBar(false)
+                    Toast.makeText(this@RegisterActivity, "Passwords do not match!", Toast.LENGTH_SHORT).show()
+                    return false
+                }
+
+            }else{
+                setProgressBar(false)
+                Toast.makeText(this@RegisterActivity, "Please fill all forms!", Toast.LENGTH_SHORT).show()
+                return false
+
+        }
+
+    }
+
+    private fun hideSoftKeyboard(view: View){
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun setProgressBar(loading: Boolean){
+        if(loading){
+            binding.pbRegister.visibility = View.VISIBLE
+        }else{
+            binding.pbRegister.visibility = View.GONE
+        }
     }
 }
