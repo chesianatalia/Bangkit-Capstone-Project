ffadb9f46e5a41064d0524acf695a7dfcb4214e4
hnsfhrzi <hans13fahrezi@gmail.com>
2022-05-25 14:25:56 +0700

    Update NavDraw
    MASIH BELUM BISA T^T

---
+++ app/src/main/java/com/chesia/bangkitcapstoneproject/LoginViewModel.kt
@@ -0,0 +1,19 @@
+package com.chesia.bangkitcapstoneproject
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.chesia.bangkitcapstoneproject.Local.UserPreferences
+import com.chesia.bangkitcapstoneproject.Networking.LoginResponseData
+
+
+class LoginViewModel(private val pref: UserPreferences) : ViewModel(){
+//    fun saveToken(user:LoginResponseData){
+//        viewModelScope.launch{
+//            pref.saveUser(LoginResponseData(user.token,user.endpoint))
+//        }
+//    }
+//
+//    fun getUser(): LiveData<LoginResponseData>{
+//        return pref.getUser().asLiveData()
+//    }
+}
