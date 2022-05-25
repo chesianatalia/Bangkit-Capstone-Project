e94406580c5bd8c3da93f7c4de5e72cfdab372af
ailatan aisehc <89681954+chesianatalia@users.noreply.github.com>
2022-05-25 15:12:52 +0700

    fixing something

--- app/src/main/java/com/chesia/bangkitcapstoneproject/Local/UserPreferences.kt
+++ app/src/main/java/com/chesia/bangkitcapstoneproject/Local/UserPreferences.kt
@@ -1,33 +1,27 @@
 package com.chesia.bangkitcapstoneproject.Local
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.booleanPreferencesKey
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.stringPreferencesKey
-import com.chesia.bangkitcapstoneproject.Networking.LoginResponseData
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
-import android.content.Context
-class UserPreferences (context: Context){
 
+import android.content.Context
+
+class UserPreferences (context: Context) {
     private val preferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
     val editor = preferences.edit()
 
-    fun setToken(value: String){
+    fun setToken(value: String) {
         editor.putString(TOKEN, value)
         editor.apply()
     }
 
-    fun getToken(): String{
-        val token: String = if(preferences.getString(TOKEN, "") != null) preferences.getString(TOKEN, "")!! else ""
+    fun getToken(): String {
+        val token: String =
+            if (preferences.getString(TOKEN, "") != null) preferences.getString(TOKEN, "")!! else ""
         return token
 
     }
-  fun clearPreference(){
+
+    fun clearPreference() {
         editor.clear()
         editor.apply()
-
-
+    }
 
     companion object{
         private const val PREFS_NAME = "user_pref"
