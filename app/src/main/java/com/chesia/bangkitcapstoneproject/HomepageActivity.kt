e548aaee8d3b0ca054d408dec213a8a7e309e5fc
hnsfhrzi <hans13fahrezi@gmail.com>
2022-06-04 14:18:21 +0700

    Fixing and adding info
    Adding some info in Homepage

--- app/src/main/java/com/chesia/bangkitcapstoneproject/HomepageActivity.kt
+++ app/src/main/java/com/chesia/bangkitcapstoneproject/HomepageActivity.kt
@@ -18,6 +18,7 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.bumptech.glide.Glide
 import com.chesia.bangkitcapstoneproject.Local.LoginPreferences
 import com.chesia.bangkitcapstoneproject.Networking.ApiConfig
 import com.chesia.bangkitcapstoneproject.Networking.UserProfileResponse
@@ -152,9 +153,13 @@ class HomepageActivity : AppCompatActivity() {
             ) {
                 if(response.isSuccessful){
                     Log.d("Response", response.body()?.success.toString())
-                    binding.tvUserName.text = response.body()?.data?.user?.fullname
-                    binding.tvUserPhoneNumber.text = response.body()?.data?.user?.meta?.phone
-                    binding.tvUserEmail.text = response.body()?.data?.user?.email
+                    binding.apply {
+//                        Glide.with(this@HomepageActivity).load(response.body()?.data?.user?.photoUrl).circleCrop().into(binding.imgUser)
+                        tvUserName.text = response.body()?.data?.user?.fullname
+                        tvUserPhoneNumber.text = response.body()?.data?.user?.meta?.phone
+                        tvUserEmail.text = response.body()?.data?.user?.email
+                        tvUserPoint.text = "${response.body()?.data?.user?.status} points"
+                    }
                 }
             }
 
